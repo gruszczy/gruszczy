c88ef9c70b9482322a9d3da82d76aceccfe6d9f7
Filip Gruszczynski <gruszczy@gmail.com>
2025-09-28 16:25:06 -0700

    Clean up things generated by create docusaurus tool.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,12 +42,10 @@ const config: Config = {
       'classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        blog: false,
         theme: {
           customCss: './src/css/custom.css',
         },
